15a5fcd1976243a350d13b51e5d32497ea2b0227
Ryan Nett <JNett96@gmail.com>
2021-06-25 17:11:05 -0700

    Add Bearer auth scopes, change Basic auth scope to use Ktor's credential class
    Signed-off-by: Ryan Nett <JNett96@gmail.com>

--- plugins/ktor-client/krosstalk-ktor-client-auth/src/commonMain/kotlin/com/rnett/krosstalk/ktor/client/auth/Auth.kt
+++ plugins/ktor-client/krosstalk-ktor-client-auth/src/commonMain/kotlin/com/rnett/krosstalk/ktor/client/auth/Auth.kt
@@ -1,16 +1,15 @@
 package com.rnett.krosstalk.ktor.client.auth
 
 import com.rnett.krosstalk.ScopeInstance
-import com.rnett.krosstalk.client.invoke
 import com.rnett.krosstalk.ktor.client.KtorClientScope
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.features.auth.Auth
+import io.ktor.client.features.auth.providers.BasicAuthCredentials
+import io.ktor.client.features.auth.providers.BearerAuthConfig
+import io.ktor.client.features.auth.providers.BearerTokens
 import io.ktor.client.features.auth.providers.basic
-
-/**
- * Credentials for basic auth.
- */
-public data class BasicCredentials(val username: String, val password: String)
+import io.ktor.client.features.auth.providers.bearer
+import io.ktor.client.statement.HttpResponse
 
 /**
  * A base Ktor client authentication scope.
@@ -18,7 +17,7 @@ public data class BasicCredentials(val username: String, val password: String)
 public abstract class KtorClientAuth<D> : KtorClientScope<D> {
     public abstract fun Auth.configureClientAuth(data: D)
 
-    override fun HttpClientConfig<*>.configureClient(data: D) {
+    final override fun HttpClientConfig<*>.configureClient(data: D) {
         Auth {
             configureClientAuth(data)
         }
@@ -29,12 +28,11 @@ public abstract class KtorClientAuth<D> : KtorClientScope<D> {
  * A Ktor client Basic authentication scope.
  */
 public open class KtorClientBasicAuth(public val sendWithoutRequest: Boolean = true, public val realm: String? = null) :
-    KtorClientAuth<BasicCredentials>() {
-    override fun Auth.configureClientAuth(data: BasicCredentials) {
+    KtorClientAuth<BasicAuthCredentials>() {
+    final override fun Auth.configureClientAuth(data: BasicAuthCredentials) {
         basic {
-            sendWithoutRequest = this@KtorClientBasicAuth.sendWithoutRequest
-            username = data.username
-            password = data.password
+            credentials { data }
+            sendWithoutRequest { this@KtorClientBasicAuth.sendWithoutRequest }
             realm = this@KtorClientBasicAuth.realm
         }
     }
@@ -44,4 +42,62 @@ public open class KtorClientBasicAuth(public val sendWithoutRequest: Boolean = t
  * Create a basic auth scope instance by providing the username and password.
  */
 public operator fun <T : KtorClientBasicAuth> T.invoke(username: String, password: String): ScopeInstance<T> =
-    this.invoke(BasicCredentials(username, password))
+    this.invoke(BasicAuthCredentials(username, password))
+
+/**
+ * A Ktor client Bearer authentication scope that loads tokens based on passed data.
+ */
+public abstract class KtorClientBearerAuth<T>(public val sendWithoutRequest: Boolean = true, public val realm: String? = null) :
+    KtorClientAuth<T>() {
+
+    /**
+     * Used as Ktor's [BearerAuthConfig.loadTokens].
+     */
+    public abstract suspend fun loadTokens(data: T): BearerTokens?
+
+    /**
+     * Used as Ktor's [BearerAuthConfig.refreshTokens].  Calls [loadTokens] by default.
+     */
+    public open suspend fun refreshTokens(data: T, response: HttpResponse): BearerTokens? = loadTokens(data)
+
+    final override fun Auth.configureClientAuth(data: T) {
+        bearer {
+            this.loadTokens{ loadTokens(data) }
+            this.refreshTokens{ refreshTokens(data, it) }
+            sendWithoutRequest { this@KtorClientBearerAuth.sendWithoutRequest }
+            realm = this@KtorClientBearerAuth.realm
+        }
+    }
+}
+
+/**
+ * Bearer auth tokens, similar to [BearerTokens] except the refresh token is optional.
+ */
+public data class BearerAuthTokens(val accessToken: String, val refreshToken: String?){
+    /**
+     * Get the Ktor access tokens, returning an empty refresh token if [refreshToken] is `null`.
+     */
+    public fun accessTokens(): BearerTokens = BearerTokens(accessToken, refreshToken.orEmpty())
+
+    /**
+     * Get the Ktor refresh tokens, returning `null` if [refreshToken] is `null`.
+     */
+    public fun refreshTokens(): BearerTokens? = refreshToken?.let { accessTokens() }
+}
+
+/**
+ * A Ktor client Bearer authentication scope that is passed tokens.
+ */
+public open class KtorClientBearerTokenAuth(sendWithoutRequest: Boolean = true, realm: String? = null):
+    KtorClientBearerAuth<BearerAuthTokens>(sendWithoutRequest, realm){
+    final override suspend fun loadTokens(data: BearerAuthTokens): BearerTokens = data.accessTokens()
+
+    final override suspend fun refreshTokens(data: BearerAuthTokens, response: HttpResponse): BearerTokens? =
+        data.refreshTokens()
+}
+
+/**
+ * Create a bearer auth scope instance by providing the access token and optionally a refresh token.
+ */
+public operator fun <T : KtorClientBearerTokenAuth> T.invoke(accessToken: String, refreshToken: String? = null): ScopeInstance<T> =
+    this.invoke(BearerAuthTokens(accessToken, refreshToken))
