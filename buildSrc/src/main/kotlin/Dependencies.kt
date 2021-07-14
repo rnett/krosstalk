a0016832164bfc6c27eff2a5b0307ee1b90635c7
Ryan Nett <JNett96@gmail.com>
2021-07-14 15:46:01 -0700

    Update coroutines to 1.5.1, Kotlin to 1.5.21 (#24)
    * Update coroutines to 1.5.1, Kotlin to 1.5.21
    
    Signed-off-by: Ryan Nett <JNett96@gmail.com>
    
    * Update compiler plugin utils for new Kotlin
    
    Signed-off-by: Ryan Nett <JNett96@gmail.com>
    
    * Update changelog
    
    Signed-off-by: Ryan Nett <JNett96@gmail.com>

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,5 @@
 object Dependencies {
-    const val kotlin = "1.5.20"
+    const val kotlin = "1.5.21"
     const val serialization = "1.2.2"
     const val dokka = "1.5.0"
 
@@ -7,7 +7,7 @@ object Dependencies {
 
     const val autoService = "1.0-rc6"
 
-    const val compilerPluginUtils = "1.0.1"
+    const val compilerPluginUtils = "1.0.2"
 
     const val gradlePluginPublish = "0.15.0"
     const val buildconfig = "3.0.0"
