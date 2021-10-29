f993e55c52baf8e1663cb4c9f0462cc765cd6495
Ryan Nett <JNett96@gmail.com>
2021-10-28 22:27:09 -0700

    Add workaround for KT-49360
    Signed-off-by: Ryan Nett <JNett96@gmail.com>

--- compiler/krosstalk-gradle-plugin/src/main/kotlin/com/rnett/krosstalk/KrosstalkGradlePlugin.kt
+++ compiler/krosstalk-gradle-plugin/src/main/kotlin/com/rnett/krosstalk/KrosstalkGradlePlugin.kt
@@ -5,10 +5,13 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerPluginSupportPlugin
 import org.jetbrains.kotlin.gradle.plugin.SubpluginArtifact
 import org.jetbrains.kotlin.gradle.plugin.SubpluginOption
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 class KrosstalkGradlePlugin : KotlinCompilerPluginSupportPlugin {
 
     override fun applyToCompilation(kotlinCompilation: KotlinCompilation<*>): Provider<List<SubpluginOption>> {
+        //TODO workaround for https://youtrack.jetbrains.com/issue/KT-49360
+        kotlinCompilation.kotlinOptions.freeCompilerArgs += "-Xverify-compiler=false"
         val project = kotlinCompilation.target.project
         return project.provider { emptyList() }
     }
